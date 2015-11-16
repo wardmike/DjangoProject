8ae230d7a3be0e23d3eede89222c9ba95243577f
Michael <xenophon.of.athens@gmail.com>
2015-11-15 23:14:41 -0700

    added home url

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
+	url(r'^$', 'flashcards.views.home', name="home"),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
+
