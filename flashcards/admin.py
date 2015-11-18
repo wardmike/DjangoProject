88c7cdc7d77922ebb3914ccb64cf6d0dcdd9e4ed
Michael <xenophon.of.athens@gmail.com>
2015-11-17 20:33:15 -0700

    added forms

--- flashcards/admin.py
+++ flashcards/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .forms import FlashcardForm
+from .forms import UserForm
+from .models import User
+from .models import Flashcard
+
+class FlashcardAdmin(admin.ModelAdmin):
+	list_display = ["english_word", "spanish_word"]
+	form = FlashcardForm
+	#class Meta:
+	#	model = Flashcard
+
+class UserAdmin(admin.ModelAdmin):
+	form = UserForm
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Flashcard, FlashcardAdmin)
