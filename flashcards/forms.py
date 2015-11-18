d808f747685805487dc2e383bf6e316cdb5ce147
Michael <xenophon.of.athens@gmail.com>
2015-11-17 20:46:56 -0700

    added form validation

--- flashcards/forms.py
+++ flashcards/forms.py
@@ -12,3 +12,17 @@ class UserForm(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ['user_name', 'email']
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		email_base, provider = email.split("@")
+		domain, extension = provider.split(".")
+		#Validation example:
+		#if not extension == "edu":
+		#	raise forms.ValidationError("Please use a valid .edu email address.")
+		return email
+
+	def clean_user_name(self):
+		user_name = self.cleaned_data.get('user_name')
+		#enter validation code here
+		return user_name
