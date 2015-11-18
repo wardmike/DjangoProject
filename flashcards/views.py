8128f42cd3859d5937e6880f835e6ecd9f99b53d
Michael <xenophon.of.athens@gmail.com>
2015-11-17 21:05:53 -0700

    added dynamic view

--- flashcards/views.py
+++ flashcards/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, "home.html", {})
+	if request.user.is_authenticated():
+		title = "Welcome, %s!" %(request.user)
+	else:
+		title = "Welcome!"
+
+	#add a form
+	context = {
+		"title": title,
+	}
+	return render(request, "home.html", context)
