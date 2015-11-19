f876935e3546c6ea1f9cf30a89087e0b59980c11
Michael <xenophon.of.athens@gmail.com>
2015-11-18 18:33:03 -0700

    added email to contact form

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "djangoproject2015@gmail.com"
+EMAIL_HOST_PASSWORD = "djangoproject$"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 # Application definition
 
 INSTALLED_APPS = (
