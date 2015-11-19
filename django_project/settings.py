6327cee4fafc6f3461c4f001c7969678c32ba5f4
Michael <xenophon.of.athens@gmail.com>
2015-11-18 19:05:32 -0700

    added static and media

--- django_project/settings.py
+++ django_project/settings.py
@@ -105,3 +105,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static", "our_static"),
+    #'/var/www/static/',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
