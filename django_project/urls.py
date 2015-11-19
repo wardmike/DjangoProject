e551b9b222641090e591bebc3904d96e1555e9db
Michael <xenophon.of.athens@gmail.com>
2015-11-18 19:21:08 -0700

    serving static files

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -7,3 +9,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
